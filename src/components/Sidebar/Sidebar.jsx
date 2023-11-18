91654c44d2da880de9bbcf9bd5851bcee7d66f2e
itihito <itihito>
2023-11-19 08:08:21 +0900

    ページジャンプボタンをリファクタリング

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,102 +1,43 @@
-// import React from "react";
 import "./Sidebar.css";
 import PropTypes from "prop-types";
 
+function generatePageJumpButton(offset, limit, handlePageJump) {
+  return (
+    <li key={offset}>
+      <button
+        className="pageJumpBtn"
+        onClick={() => handlePageJump(offset, limit)}
+      >
+        {`No.${offset + 1} ~ ${offset + limit}`}
+      </button>
+    </li>
+  );
+}
+
 function Sidebar({ handlePageJump }) {
+  const pageRanges = [
+    { offset: 0, limit: 100 },
+    { offset: 100, limit: 100 },
+    { offset: 200, limit: 100 },
+    { offset: 300, limit: 100 },
+    { offset: 400, limit: 100 },
+    { offset: 500, limit: 100 },
+    { offset: 600, limit: 100 },
+    { offset: 700, limit: 100 },
+    { offset: 800, limit: 100 },
+    { offset: 900, limit: 100 },
+    { offset: 1000, limit: 10 },
+  ];
+
   return (
     <div className="sidebar">
       <div className="sidebar-no">
         <h3>図鑑No</h3>
       </div>
       <ul>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(0, 100)}
-          >
-            No.1 ~ 100
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(100, 100)}
-          >
-            No.101 ~ 200
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(200, 100)}
-          >
-            No.201 ~ 300
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(300, 100)}
-          >
-            No.301 ~ 400
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(400, 100)}
-          >
-            No.401 ~ 500
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(500, 100)}
-          >
-            No.501 ~ 600
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(600, 100)}
-          >
-            No.601 ~ 700
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(700, 100)}
-          >
-            No.701 ~ 800
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(800, 100)}
-          >
-            No.801 ~ 900
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(900, 100)}
-          >
-            No.901 ~ 1000
-          </button>
-        </li>
-        <li>
-          <button
-            className="pageJumpBtn"
-            onClick={() => handlePageJump(1000, 10)}
-          >
-            No.1001 ~ 1010
-          </button>
-        </li>
+        {pageRanges.map(({ offset, limit }) =>
+          generatePageJumpButton(offset, limit, handlePageJump)
+        )}
       </ul>
     </div>
   );
